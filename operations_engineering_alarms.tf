a073ce87046d1a27b2200a422efd9479341b8e03
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-10-01 15:02:52 +0100

    restructure terraform

--- operations_engineering_alarms.tf
+++ operations_engineering_alarms.tf
@@ -1,3 +1,5 @@
+# Alarm for when user levgorbunov1 cancels any GitHub Actions workflow
+
 resource "aws_cloudwatch_log_metric_filter" "levgorbunov1_cancel_workflow_filter" {
   name           = "Levgorbunov1CancelWorkflowFilter"
   log_group_name = data.aws_cloudwatch_log_group.github_events_log_group.name
