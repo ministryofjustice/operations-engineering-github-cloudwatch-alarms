424f2572f299849071c8196f8f9ae40682f15c9b
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-10-01 16:03:12 +0100

    configure cloudwatch alarm with sns topic

--- operations_engineering_alarms.tf
+++ operations_engineering_alarms.tf
@@ -12,3 +12,20 @@ resource "aws_cloudwatch_log_metric_filter" "levgorbunov1_cancel_workflow_filter
     value     = "1"
   }
 }
+
+resource "aws_sns_topic" "operations_engineering_github_alerts_topic" {
+  name = "operations-engineering-github-alerts"
+}
+
+resource "aws_cloudwatch_metric_alarm" "levgorbunov1_cancel_workflow_alarm" {
+  alarm_name                = "Levgorbunov1CancelWorkflowAlarm"
+  comparison_operator       = "GreaterThanThreshold"
+  evaluation_periods        = 1
+  metric_name               = aws_cloudwatch_log_metric_filter.levgorbunov1_cancel_workflow_filter.metric_transformation[0].name
+  namespace                 = aws_cloudwatch_log_metric_filter.levgorbunov1_cancel_workflow_filter.metric_transformation[0].namespace
+  period                    = 60
+  statistic                 = "Sum"
+  threshold                 = 0
+  alarm_description         = "Alarm for when user levgorbunov1 cancels any GitHub Actions workflow"
+  alarm_actions             = [aws_sns_topic.operations_engineering_github_alerts_topic.arn]
+}
