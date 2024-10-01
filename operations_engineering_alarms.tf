e56e7ffe53b536b5c98b5e0edbc8384e15c2305e
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-10-01 16:17:08 +0100

    fmt terraform

--- operations_engineering_alarms.tf
+++ operations_engineering_alarms.tf
@@ -18,11 +18,11 @@ resource "aws_sns_topic" "operations_engineering_github_alerts_topic" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "levgorbunov1_cancel_workflow_alarm" {
-  alarm_name          = "Levgorbunov1CancelWorkflowAlarm"
-  alarm_description   = "Alarm for when user levgorbunov1 cancels any GitHub Actions workflow"
+  alarm_name        = "Levgorbunov1CancelWorkflowAlarm"
+  alarm_description = "Alarm for when user levgorbunov1 cancels any GitHub Actions workflow"
 
-  metric_name         = aws_cloudwatch_log_metric_filter.levgorbunov1_cancel_workflow_filter.metric_transformation[0].name
-  namespace           = aws_cloudwatch_log_metric_filter.levgorbunov1_cancel_workflow_filter.metric_transformation[0].namespace
+  metric_name = aws_cloudwatch_log_metric_filter.levgorbunov1_cancel_workflow_filter.metric_transformation[0].name
+  namespace   = aws_cloudwatch_log_metric_filter.levgorbunov1_cancel_workflow_filter.metric_transformation[0].namespace
 
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 1
@@ -30,5 +30,5 @@ resource "aws_cloudwatch_metric_alarm" "levgorbunov1_cancel_workflow_alarm" {
   statistic           = "Sum"
   threshold           = 0
 
-  alarm_actions       = [aws_sns_topic.operations_engineering_github_alerts_topic.arn]
+  alarm_actions = [aws_sns_topic.operations_engineering_github_alerts_topic.arn]
 }
