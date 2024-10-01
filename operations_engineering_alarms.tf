bc6ae96902983cabfabb5e8a490021c1b53ff876
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-10-01 16:13:27 +0100

    update metric filter

--- operations_engineering_alarms.tf
+++ operations_engineering_alarms.tf
@@ -4,7 +4,7 @@ resource "aws_cloudwatch_log_metric_filter" "levgorbunov1_cancel_workflow_filter
   name           = "Levgorbunov1CancelWorkflowFilter"
   log_group_name = data.aws_cloudwatch_log_group.github_events_log_group.name
 
-  pattern = "[INFO, timestamp, requestId, { $.eventName = \"workflows.cancel_workflow_run\" && $.userIdentity.principalId = \"levgorbunov1\" }]"
+  pattern = "[INFO, timestamp, requestId, $.eventName = \"workflows.cancel_workflow_run\" && $.userIdentity.principalId = \"levgorbunov1\"]"
 
   metric_transformation {
     name      = "Levgorbunov1CancelWorkflowEvents"
