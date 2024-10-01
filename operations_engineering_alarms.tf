9c38776d5c7c683e1fe0a3eeec129374ad3143c5
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-10-01 16:16:13 +0100

    update filter

--- operations_engineering_alarms.tf
+++ operations_engineering_alarms.tf
@@ -4,7 +4,7 @@ resource "aws_cloudwatch_log_metric_filter" "levgorbunov1_cancel_workflow_filter
   name           = "Levgorbunov1CancelWorkflowFilter"
   log_group_name = data.aws_cloudwatch_log_group.github_events_log_group.name
 
-  pattern = "[INFO, timestamp, requestId, $.eventName = \"workflows.cancel_workflow_run\" && $.userIdentity.principalId = \"levgorbunov1\"]"
+  pattern = "[INFO, timestamp, requestId, *]  ?\"eventName\"=\"workflows.cancel_workflow_run\" ?\"userIdentity.principalId\"=\"levgorbunov1\""
 
   metric_transformation {
     name      = "Levgorbunov1CancelWorkflowEvents"
@@ -19,13 +19,16 @@ resource "aws_sns_topic" "operations_engineering_github_alerts_topic" {
 
 resource "aws_cloudwatch_metric_alarm" "levgorbunov1_cancel_workflow_alarm" {
   alarm_name          = "Levgorbunov1CancelWorkflowAlarm"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = 1
+  alarm_description   = "Alarm for when user levgorbunov1 cancels any GitHub Actions workflow"
+
   metric_name         = aws_cloudwatch_log_metric_filter.levgorbunov1_cancel_workflow_filter.metric_transformation[0].name
   namespace           = aws_cloudwatch_log_metric_filter.levgorbunov1_cancel_workflow_filter.metric_transformation[0].namespace
+
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
   period              = 60
   statistic           = "Sum"
   threshold           = 0
-  alarm_description   = "Alarm for when user levgorbunov1 cancels any GitHub Actions workflow"
+
   alarm_actions       = [aws_sns_topic.operations_engineering_github_alerts_topic.arn]
 }
