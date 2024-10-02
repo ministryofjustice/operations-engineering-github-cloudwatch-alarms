660401d109e20a5daa1392266c632acee8bd609f
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-10-02 17:00:19 +0100

    do more string operations to properly dynamically generate query

--- modules/alarm/alarm.tf
+++ modules/alarm/alarm.tf
@@ -1,16 +1,22 @@
+locals {
+  filters = [
+    "(${join(" || ", formatlist("$.userIdentity.principalId = \"%s\"", var.metric_filter_pattern.usernames_equal_to))})",
+    "(${join(" || ", formatlist("$.userIdentity.principalId = \"%s\"", var.metric_filter_pattern.usernames_not_equal_to))})",
+    "(${join(" || ", formatlist("$.eventName = \"%s\"", var.metric_filter_pattern.events))})",
+    "(${join(" || ", formatlist("$.userIdentity.details.Repository = \"%s\"", var.metric_filter_pattern.repositories))})",
+    "(${join(" || ", formatlist("$.userIdentity.details.GitHubOrganization = \"%s\"", var.metric_filter_pattern.organisations))})"
+  ]
+
+  remove_empty_filters = [for cond in local.filters : cond if cond != "()"]
+
+  concatenated_filters = join(" && ", local.remove_empty_filters)
+}
+
 resource "aws_cloudwatch_log_metric_filter" "levgorbunov1_cancel_workflow_filter" {
   name           = var.metric_name
   log_group_name = data.aws_cloudwatch_log_group.github_events_log_group.name
 
-  pattern = <<EOF
-  {
-    ${length(var.metric_filter_pattern.usernames_equal_to) > 0 ? "(${join(" || ", formatlist("$.userIdentity.principalId = \"%s\"", var.metric_filter_pattern.usernames_equal_to))}) &&" : ""}
-    ${length(var.metric_filter_pattern.usernames_not_equal_to) > 0 ? "(${join(" || ", formatlist("$.userIdentity.principalId = \"%s\"", var.metric_filter_pattern.usernames_not_equal_to))}) &&" : ""}
-    ${length(var.metric_filter_pattern.events) > 0 ? "(${join(" || ", formatlist("$.eventName = \"%s\"", var.metric_filter_pattern.events))}) &&" : ""}
-    ${length(var.metric_filter_pattern.repositories) > 0 ? "(${join(" || ", formatlist("$.userIdentity.details.Repository = \"%s\"", var.metric_filter_pattern.repositories))}) &&" : ""}
-    ${length(var.metric_filter_pattern.organisations) > 0 ? "(${join(" || ", formatlist("$.userIdentity.details.GitHubOrganization = \"%s\"", var.metric_filter_pattern.organisations))})" : ""}
-  }
-  EOF
+  pattern = "{ ${locals.concatenated_filters} }"
 
   metric_transformation {
     name      = var.metric_name
