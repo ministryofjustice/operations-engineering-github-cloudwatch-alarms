a05fc0802f832aa6d7fd52ad6d1aca71a7f13118
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-10-02 11:48:28 +0100

    terraform fmt

--- operations_engineering_alarms.tf
+++ operations_engineering_alarms.tf
@@ -1,13 +1,13 @@
 module "levgorbunov1_cancel_workflow_alarm" {
   source = "./modules/alarm"
 
-  metric_name = "Levgorbunov1CancelWorkflowEvents"
+  metric_name           = "Levgorbunov1CancelWorkflowEvents"
   metric_filter_pattern = "{ ($.eventName = \"workflows.cancel_workflow_run\") && ($.userIdentity.principalId = levgorbunov1) }"
 
-  alarm_name = "Levgorbunov1CancelWorkflowAlarm"
+  alarm_name        = "Levgorbunov1CancelWorkflowAlarm"
   alarm_description = "Alarm for when user levgorbunov1 cancels any GitHub Actions workflow"
 
   team = "operations-engineering"
-  
+
   subscribers = ["lev.gorbunov@digital.justice.gov.uk"]
 }
