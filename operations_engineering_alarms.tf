a2a1c91e27c84da1edb9c09fcce91734b0d99e4d
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-10-02 13:49:22 +0100

    setup alarm for if anyone other than testuser1 tries to change certain settings of this repo - test alarm

--- operations_engineering_alarms.tf
+++ operations_engineering_alarms.tf
@@ -7,3 +7,21 @@ module "levgorbunov1_cancel_workflow_alarm" {
   team                  = "operations-engineering"
   subscribers           = ["lev.gorbunov@digital.justice.gov.uk"]
 }
+
+module "unauthorised_users_modify_repository_settings_alarm" {
+  source = "./modules/alarm"
+
+  metric_name           = "UnathorisedUsersModifyRepositorySettingsEvents"
+  metric_filter_pattern = <<EOF
+{
+  ($.eventName = "repo.access") || ($.eventName = "repo.add_member") || ($.eventName = "repo.change_merge_setting")
+  && 
+  ($.userIdentity.principalId != "testuser1") 
+  &&
+  ($.userIdentity.details.Repository= "ministryofjustice/operations-engineering-github-cloudwatch-alarms") 
+}
+EOF
+  alarm_description     = "Alarm for when user levgorbunov1 cancels any GitHub Actions workflow"
+  team                  = "operations-engineering"
+  subscribers           = ["lev.gorbunov@digital.justice.gov.uk"]
+}
