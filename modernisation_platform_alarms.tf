c7689ab62801f2125747e43743b5616537524a67
Kudzai Mtoko <kudzai.mtoko@digital.justice.gov.uk>
2024-12-03 17:46:57 +0000

    fixed format

--- modernisation_platform_alarms.tf
+++ modernisation_platform_alarms.tf
@@ -8,7 +8,7 @@ module "unauthorised_users_modify_repository_settings_mod_platform_alarm" {
   metric_name = "UnathorisedUsersModifyRepositorySettingsEventsModPlatform"
   metric_filter_pattern = {
     repositories = ["ministryofjustice/modernisation-platform", "ministryofjustice/modernisation-platform-terraform-ec2-instance"]
-    events       = [
+    events = [
       "repo.access",
       "repo.add_member",
       "repo.change_merge_setting",
