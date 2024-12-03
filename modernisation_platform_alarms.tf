8a068d62e07a7607c733a496503818ff8c077e4f
Kudzai Mtoko <kudzai.mtoko@digital.justice.gov.uk>
2024-12-03 12:16:16 +0000

    updated metric name

--- modernisation_platform_alarms.tf
+++ modernisation_platform_alarms.tf
@@ -5,7 +5,7 @@ module "unauthorised_users_modify_repository_settings_mod_platform_alarm" {
 
   alarm_description = "Alarm for when any user modifies the settings of ministryofjustice/modernisation-platform repository"
 
-  metric_name = "UnathorisedUsersModifyRepositorySettingsEvents"
+  metric_name = "UnathorisedUsersModifyRepositorySettingsEventsModPlatform"
   metric_filter_pattern = {
     repositories = ["ministryofjustice/modernisation-platform", "ministryofjustice/modernisation-platform-terraform-ec2-instance"]
     events       = ["repo.access", "repo.add_member", "repo.change_merge_setting"]
