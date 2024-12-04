694135ac23aee1c976d8026194003f60dfee3ecb
Kudzai Mtoko <kudzai.mtoko@digital.justice.gov.uk>
2024-12-04 16:57:06 +0000

    test

--- modernisation_platform_alarms.tf
+++ modernisation_platform_alarms.tf
@@ -5,13 +5,14 @@ module "unauthorised_users_modify_repository_settings_mod_platform_alarm" {
 
   alarm_description = "Alarm for when any user modifies the settings of ministryofjustice/modernisation-platform repository"
 
-  metric_name = "UnauthorisedUsersModifyRepositorySettingsEventsModPlatform"
+  metric_name = "UnathorisedUsersModifyRepositorySettingsEventsModPlatform"
   metric_filter_pattern = {
 
     repositories = ["ministryofjustice/modernisation-platform-terraform-ec2-instance"]
 
     events = [
-      # Branch Protection Rule Modifications
+
+      "protected_branch.update_signature_requirement_enforcement_level",
       "protected_branch.create",
       "protected_branch.destroy",
       "protected_branch.policy_override",
@@ -25,41 +26,6 @@ module "unauthorised_users_modify_repository_settings_mod_platform_alarm" {
       "protected_branch.update_required_status_checks_enforcement_level",
       "protected_branch.update_signature_requirement_enforcement_level",
 
-      # Repository Settings Changes
-      "repo.add_member",
-      "repo.change_merge_setting",
-      "repo.remove_member",
-      "repo.rename",
-      "repo.transfer",
-      "repo.transfer_start",
-      "repo.update_default_branch",
-
-      # Visibility and Access Control
-      "repository_visibility_change.disable",
-      "repository_visibility_change.enable",
-      "private_repository_forking.enable",
-
-      # Security Scanning and Vulnerabilities
-      "repository_security_configuration.removed",
-      "repository_vulnerability_alerts.disable",
-      "repository_vulnerability_alerts.enable",
-      "repository_secret_scanning.disable",
-      "repository_secret_scanning_push_protection.disable",
-      "dependabot_alerts.disable",
-      "dependabot_alerts.enable",
-      "secret_scanning.disable",
-      "secret_scanning.enable",
-
-      # Administrative Privileges
-      "business.add_admin",
-      "business.remove_admin",
-      "business.audit_log_export",
-      "business.enable_two_factor_requirement",
-      "business.disable_two_factor_requirement",
-
-      # Audit Log Modifications
-      "audit_log_streaming.create",
-      "audit_log_streaming.destroy"
     ]
   }
 }
