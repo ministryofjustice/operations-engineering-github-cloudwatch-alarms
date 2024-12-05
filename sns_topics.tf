f21bb60dabcdf113787bb4306717d760dd910914
Kudzai Mtoko <kudzai.mtoko@digital.justice.gov.uk>
2024-12-05 17:06:07 +0000

    removed code for modernisation platform

--- sns_topics.tf
+++ sns_topics.tf
@@ -4,10 +4,3 @@ module "operations_engineering_topic" {
   team        = "operations-engineering"
   subscribers = ["lev.gorbunov@digital.justice.gov.uk"]
 }
-
-module "modernisation_platform_topic" {
-  source = "./modules/sns_topic"
-
-  team        = "modernisation-platform"
-  subscribers = ["modernisation-platform@digital.justice.gov.uk", "kudzai.mtoko@digital.justice.gov.uk"]
-}
