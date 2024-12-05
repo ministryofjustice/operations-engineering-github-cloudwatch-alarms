90a0cadc1b13780306a8fa34d59a3ae07f266091
Kudzai Mtoko <kudzai.mtoko@digital.justice.gov.uk>
2024-12-05 10:15:03 +0000

    modernisation_platform_terraform_bastion_linu

--- modernisation_platform_alarms.tf
+++ modernisation_platform_alarms.tf
@@ -792,3 +792,135 @@ module "unauthorized_users_modify_repository_settings_audit_log_modernisation_pl
     ]
   }
 }
+
+#####################################################################
+# modernisation-platform-terraform-bastion-linux repo alarm events #
+#####################################################################
+
+# Branch Protection Rule Modifications
+module "unauthorized_users_modify_repository_settings_branch_protection_modernisation_platform_terraform_bastion_linux" {
+  source = "./modules/alarm"
+
+  sns_topic_arn     = module.modernisation_platform_topic.sns_topic_arn
+  alarm_description = "Alarm for branch protection rule modifications in the repository"
+  metric_name       = "BranchProtectionRuleModificationEvents-modernisation-platform-terraform-bastion-linux"
+  metric_filter_pattern = {
+    repositories = ["ministryofjustice/modernisation-platform-ami-builds"]
+
+    events = [
+      "protected_branch.create",
+      "protected_branch.destroy",
+      "protected_branch.policy_override",
+      "protected_branch.update_admin_enforced",
+      "protected_branch.update_allow_force_pushes_enforcement_level",
+      "protected_branch.update_ignore_approvals_from_contributors",
+      "protected_branch.update_lock_allows_fetch_and_merge",
+      "protected_branch.update_pull_request_reviews_enforcement_level",
+      "protected_branch.update_require_code_owner_review",
+      "protected_branch.update_required_approving_review_count",
+      "protected_branch.update_required_status_checks_enforcement_level",
+      "protected_branch.update_signature_requirement_enforcement_level"
+    ]
+  }
+}
+
+# Repository Settings Changes
+module "unauthorized_users_modify_repository_settings_repo_changes_modernisation_platform_terraform_bastion_linu" {
+  source = "./modules/alarm"
+
+  sns_topic_arn     = module.modernisation_platform_topic.sns_topic_arn
+  alarm_description = "Alarm for repository settings changes in the repository"
+  metric_name       = "RepositorySettingsChangeEvents-modernisation-platform-terraform-bastion-linux"
+  metric_filter_pattern = {
+    repositories = ["ministryofjustice/modernisation-platform-ami-builds"]
+
+    events = [
+      "repo.add_member",
+      "repo.change_merge_setting",
+      "repo.remove_member",
+      "repo.rename",
+      "repo.transfer",
+      "repo.transfer_start",
+      "repo.update_default_branch"
+    ]
+  }
+}
+
+# Visibility and Access Control
+module "unauthorized_users_modify_repository_settings_visibility_modernisation_platform_terraform_bastion_linu" {
+  source = "./modules/alarm"
+
+  sns_topic_arn     = module.modernisation_platform_topic.sns_topic_arn
+  alarm_description = "Alarm for visibility and access control changes in the repository"
+  metric_name       = "VisibilityAccessControlChangeEvents-modernisation-platform-terraform-bastion-linux"
+  metric_filter_pattern = {
+    repositories = ["ministryofjustice/modernisation-platform-ami-builds"]
+
+    events = [
+      "repository_visibility_change.disable",
+      "repository_visibility_change.enable",
+      "private_repository_forking.enable"
+    ]
+  }
+}
+
+# Security Scanning and Vulnerabilities
+module "unauthorized_users_modify_repository_settings_security_modernisation_platform_terraform_bastion_linu" {
+  source = "./modules/alarm"
+
+  sns_topic_arn     = module.modernisation_platform_topic.sns_topic_arn
+  alarm_description = "Alarm for security scanning and vulnerabilities changes in the repository"
+  metric_name       = "SecurityScanningVulnerabilitiesChangeEvents-modernisation-platform-terraform-bastion-linux"
+  metric_filter_pattern = {
+    repositories = ["ministryofjustice/modernisation-platform-ami-builds"]
+
+    events = [
+      "repository_security_configuration.removed",
+      "repository_vulnerability_alerts.disable",
+      "repository_vulnerability_alerts.enable",
+      "repository_secret_scanning.disable",
+      "repository_secret_scanning_push_protection.disable",
+      "dependabot_alerts.disable",
+      "dependabot_alerts.enable",
+      "secret_scanning.disable",
+      "secret_scanning.enable"
+    ]
+  }
+}
+
+# Administrative Privileges
+module "unauthorized_users_modify_repository_settings_admin_privileges_modernisation_platform_terraform_bastion_linu" {
+  source = "./modules/alarm"
+
+  sns_topic_arn     = module.modernisation_platform_topic.sns_topic_arn
+  alarm_description = "Alarm for administrative privilege changes in the repository"
+  metric_name       = "AdminPrivilegesChangeEvents-modernisation-platform-terraform-bastion-linux"
+  metric_filter_pattern = {
+    repositories = ["ministryofjustice/modernisation-platform-ami-builds"]
+
+    events = [
+      "business.add_admin",
+      "business.remove_admin",
+      "business.audit_log_export",
+      "business.enable_two_factor_requirement",
+      "business.disable_two_factor_requirement"
+    ]
+  }
+}
+
+# Audit Log Modifications
+module "unauthorized_users_modify_repository_settings_audit_log_modernisation_platform_terraform_bastion_linu" {
+  source = "./modules/alarm"
+
+  sns_topic_arn     = module.modernisation_platform_topic.sns_topic_arn
+  alarm_description = "Alarm for audit log modifications in the repository"
+  metric_name       = "AuditLogModificationEvents-modernisation-platform-terraform-bastion-linux"
+  metric_filter_pattern = {
+    repositories = ["ministryofjustice/modernisation-platform-ami-builds"]
+
+    events = [
+      "audit_log_streaming.create",
+      "audit_log_streaming.destroy"
+    ]
+  }
+}
