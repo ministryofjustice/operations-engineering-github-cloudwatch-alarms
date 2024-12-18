deca4d060928655945da3bb1877bbd5d2ed2b141
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-12-18 14:14:30 +0000

    remove replace

--- modules/alarm/eventbridge.tf
+++ modules/alarm/eventbridge.tf
@@ -30,7 +30,7 @@ resource "aws_cloudwatch_event_target" "default" {
   "snsTopicArn": "${var.sns_topic_arn}",
   "time": <time>,
   "period": ${var.period * 1000},
-  "queryString": "${jsonencode(replace(aws_cloudwatch_log_metric_filter.default.pattern, "\"", "\\\""))}",
+  "queryString": "${aws_cloudwatch_log_metric_filter.default.pattern}",
 }
 TEMPLATE
   }
