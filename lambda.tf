a73889fd5078410e83bd0688cca144de2d44ee2a
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-12-18 10:30:51 +0000

    include lambda source hash

--- lambda.tf
+++ lambda.tf
@@ -14,8 +14,9 @@ resource "aws_lambda_function" "alarm_handler_lambda" {
   runtime       = "python3.11"
   handler       = "extract_cloudwatch_event.lambda_handler"
 
-  role     = aws_iam_role.alarm_handler_lambda_role.arn
-  filename = data.archive_file.lambda_zip.output_path
+  role             = aws_iam_role.alarm_handler_lambda_role.arn
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 
   environment {
     variables = {
