de5b169c012e4a82e761af0e8d515dac17f8038c
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-12-18 14:36:13 +0000

    revert back to using input parameter

--- modules/alarm/eventbridge.tf
+++ modules/alarm/eventbridge.tf
@@ -14,23 +14,14 @@ resource "aws_cloudwatch_event_rule" "default" {
 }
 
 resource "aws_cloudwatch_event_target" "default" {
-  rule = aws_cloudwatch_event_rule.default.name
-  arn  = var.lambda_arn
-
-  input_transformer {
-    input_paths = {
-      time         = "test"
-    }
-
-    input_template = <<TEMPLATE
-{
-  "alarmName": ${aws_cloudwatch_metric_alarm.default.alarm_name},
-  "logGroupName": "${var.log_group_name}",
-  "snsTopicArn": "${var.sns_topic_arn}",
-  "time": "test",
-  "period": ${var.period * 1000},
-  "queryString": "${aws_cloudwatch_log_metric_filter.default.pattern}",
-}
-TEMPLATE
-  }
+  rule      = aws_cloudwatch_event_rule.default.name
+  arn       = var.lambda_arn
+  input     = jsonencode({
+    alarmName     = aws_cloudwatch_metric_alarm.default.alarm_name,
+    logGroupName  = var.log_group_name,
+    queryString   = aws_cloudwatch_log_metric_filter.default.pattern,
+    snsTopicArn   = var.sns_topic_arn,
+    time          = "$.time",
+    period        = var.period * 1000
+  })
 }
